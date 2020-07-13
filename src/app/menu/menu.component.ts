f37998c44df435080ef9849f1dc7d58f2141b0d7
sksami3 <sksami4456@gmail.com>
2020-07-13 22:21:30 +0600

    data binding

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,49 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Dish } from 'src/app/Shared/dish';
+import { Dish } from '../Shared/dish';
+import { DISHES } from '../Shared/dishes';
 
-const Dishes : Array<Dish> = [
-  {
-    id: '0',
-    name: 'Uthappizza',
-    image: '/assets/images/uthappizza.png',
-    category: 'mains',
-    featured: true,
-    label: 'Hot',
-    price: '4.99',
-    // tslint:disable-next-line:max-line-length
-    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
-  },
-  {
-    id: '1',
-    name: 'Zucchipakoda',
-    image: '/assets/images/zucchipakoda.png',
-    category: 'appetizer',
-    featured: false,
-    label: '',
-    price: '1.99',
-    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
-  },
-  {
-    id: '2',
-    name: 'Vadonut',
-    image: '/assets/images/vadonut.png',
-    category: 'appetizer',
-    featured: false,
-    label: 'New',
-    price: '1.99',
-    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
-  },
-  {
-    id: '3',
-    name: 'ElaiCheese Cake',
-    image: '/assets/images/elaicheesecake.png',
-    category: 'dessert',
-    featured: false,
-    label: '',
-    price: '2.99',
-    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
-  }
-];
 
 @Component({
   selector: 'app-menu',
@@ -55,12 +13,17 @@ const Dishes : Array<Dish> = [
 
 export class MenuComponent implements OnInit {
   
-  dishes: Array<Dish> = Dishes;
+  dishes: Array<Dish> = DISHES;
 
-  selectedDish = Dishes[0];
+  selectedDish : Dish;
 
 
   constructor() { }
+
+  clickedDish(d : Dish){
+    console.log(d);
+    this.selectedDish = d;
+  }
 
   ngOnInit(): void {
   }
