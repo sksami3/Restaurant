9f10fc3b8bc10d109ecc37ee11c55fe19f8c6625
sksami3 <sksami4456@gmail.com>
2020-07-24 16:21:25 +0600

    reactive validation done

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormControl,FormGroup, FormBuilder, Validators } from '@angular/forms';
-import {Feedback,ContactType} from '../Shared/feedback'
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Feedback, ContactType } from '../Shared/feedback'
 
 
 @Component({
@@ -10,9 +10,9 @@ import {Feedback,ContactType} from '../Shared/feedback'
 })
 export class ContactComponent implements OnInit {
   @ViewChild('fform') feedbackFormDirective;
-  
-  feedbackForm : FormGroup;
-  feedback : Feedback;
+
+  feedbackForm: FormGroup;
+  feedback: Feedback;
   contactType = ContactType;
 
   constructor(private fb: FormBuilder) {
@@ -20,21 +20,71 @@ export class ContactComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.feedbackForm.valueChanges.subscribe((data) => this.onValueChange(data));
+    this.onValueChange();
   }
 
-  createFormGroup(){
+  createFormGroup() {
     this.feedbackForm = this.fb.group({
-      firstname : ['', Validators.required],
-      lastname : ['', Validators.required],
-      telnum: ['+88', Validators.required],
-      email: ['', Validators.required],
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      telnum: ['88', [Validators.required, Validators.pattern]],
+      email: ['', [Validators.required, Validators.email]],
       agree: false,
       contacttype: 'None',
       message: ''
     });
+  };
+
+  formErrors = {
+    'firstname': '',
+    'lastname': '',
+    'telnum': '',
+    'email': ''
+  };
+
+  validationMessages = {
+    'firstname': {
+      'required': 'First name is required...',
+      'minlength': 'Must give at least 2 digit first name',
+      'maxlength': 'First name can not exceed 25 words'
+    },
+    'lastname': {
+      'required': 'Last name is required',
+      'minlength': 'Must give at least 2 digit last name',
+      'maxlength': 'Last name can not exceed 25 words'
+    },
+    'telnum': {
+      'required': 'Tel. number is required.',
+      'pattern': 'Tel. number must contain only numbers.'
+    },
+    'email': {
+      'required': 'Email is required.',
+      'email': 'Email not in valid format.'
+    },
+  };
+
+  onValueChange(data?: any) {
+    if (!this.feedbackForm) { return; }
+    const form = this.feedbackForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
   }
 
-  onSubmit(){
+  onSubmit() {
     this.feedback = this.feedbackForm.value;
     console.log(this.feedback);
     this.feedbackForm.reset({
