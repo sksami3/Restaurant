2cabb1d9839a8aa46c191b5acd84af257d841b6f
sksami3 <sksami4456@gmail.com>
2020-07-30 14:49:58 +0600

    some test cases written

--- Restaurant/e2e/src/app.e2e-spec.ts
+++ Restaurant/e2e/src/app.e2e-spec.ts
@@ -8,10 +8,6 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Restaurant app is running!');
-  });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
@@ -20,4 +16,33 @@ describe('workspace-project App', () => {
       level: logging.Level.SEVERE,
     } as logging.Entry));
   });
+
+  // it('should display message saying Restaurant', () => {
+  //   page.navigateTo('/');
+  //   expect(page.getParagraphText('app-root h1')).toEqual('Developers Restaurant');
+  // });
+
+  it('should navigate to about us page by clicking on the link', () => {
+    page.navigateTo('/');
+
+    const navlink = page.getAllElements('a').get(1);
+    navlink.click();
+
+    expect(page.getParagraphText('h3')).toBe('About Us');
+  });
+
+  // it('should enter a new comment for the first dish', () => {
+  //   page.navigateTo('/dishdetail/0');
+
+  //   const newAuthor = page.getElement('input[type=text]');
+  //   newAuthor.sendKeys('Test Author');
+
+  //   const newComment = page.getElement('textarea');
+  //   newComment.sendKeys('Test Comment');
+
+  //   const newSubmitButton = page.getElement('button[type=submit]');
+  //   newSubmitButton.click();
+
+  //   browser.pause();
+  // });
 });
--- Restaurant/e2e/src/app.po.ts
+++ Restaurant/e2e/src/app.po.ts
@@ -1,11 +1,19 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo(): Promise<unknown> {
-    return browser.get(browser.baseUrl) as Promise<unknown>;
+  navigateTo(link: string) {
+    return browser.get(link);
   }
 
-  getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  getParagraphText(selector: string) {
+    return element(by.css(selector)).getText();
+  }
+
+  getElement(selector: string) {
+    return element(by.css(selector));
+  }
+
+  getAllElements(selector: string) {
+    return element.all(by.css(selector));
   }
 }
