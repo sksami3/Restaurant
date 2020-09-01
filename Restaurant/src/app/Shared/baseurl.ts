f2c02f57febe5ee4155195f958809faf8473fec2
sksami3 <sksami4456@gmail.com>
2020-09-02 01:11:20 +0600

    delete done

--- Restaurant/src/app/Shared/baseurl.ts
+++ Restaurant/src/app/Shared/baseurl.ts
@@ -1,2 +1,2 @@
-//export const baseURL  = "https://localhost:44306/";
-export const baseURL  = "http://localhost:3000/";
+export const baseURL  = "https://localhost:44306/";
+//export const baseURL  = "http://localhost:3000/";
